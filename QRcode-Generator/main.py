f7d2b9b523e9b345fea02ccfd91f2f2dde9e0a84
Ayoub-Benanberg <ayoubvufci@gmail.com>
2024-02-23 20:32:08 +0100

    changes

--- QRcode-Generator/main.py
+++ QRcode-Generator/main.py
@@ -6,11 +6,11 @@ app.title("Qr-code-generator")
 app.geometry("400x400")
 
 def get_entry():
-    qr = pyqrcode.create(str(Mydata.get()))
-    qr.png('Mycode.png', scale=6) # scale taille dQr-code generated
+    qr = pyqrcode.create(Mydata.get())
+    # qr.png('Mycode.png', scale=6) # scale taille dQr-code generated
     
     # Load the generated qr-code image
-    qr_image = PhotoImage(file='Mycode.png')
+    qr_image = PhotoImage(file='Mycode.png') #PhotoImage : Create an image with NAME.
     
     # Updati the qr-code labl with img
     qr_label.config(image=qr_image)
