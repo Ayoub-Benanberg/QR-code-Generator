3c3b58168b63622ea594e886f114dd47535c9d9a
Ayoub-Benanberg <ayoubvufci@gmail.com>
2024-02-23 20:41:31 +0100

    removing the code in the bottom of the window

--- QRcode-Generator/main.py
+++ QRcode-Generator/main.py
@@ -3,19 +3,15 @@ from tkinter import *
 
 app = Tk()
 app.title("Qr-code-generator")
-app.geometry("400x400")
+app.geometry("400x250")
 
 def get_entry():
     qr = pyqrcode.create(Mydata.get())
-    # qr.png('Mycode.png', scale=6) # scale taille dQr-code generated
+    qr.png('Mycode.png', scale=6) # scale taille dQr-code generated
     
     # Load the generated qr-code image
     qr_image = PhotoImage(file='Mycode.png') #PhotoImage : Create an image with NAME.
-    
-    # Updati the qr-code labl with img
-    qr_label.config(image=qr_image)
-    qr_label.image = qr_image
-    
+
 Mydata = StringVar()
 
 MyLbl = Label(app, text="Write Something", font=20, pady=20)
@@ -25,10 +21,7 @@ MyEntry = Entry(app, textvariable=Mydata)
 MyEntry.pack(pady=15, ipadx=5, ipady=5)
 
 btna = Button(app, text="Generate QR Code", command=get_entry)
-btna.pack(ipady=5, ipadx=7)
+btna.pack(ipadx=7)
 
-#Afficher l'Qr-code generated
-qr_label = Label(app)
-qr_label.pack(pady=5)
 
 app.mainloop()
